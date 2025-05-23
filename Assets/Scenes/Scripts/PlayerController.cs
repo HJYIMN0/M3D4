8aa5314ed4fdecbb0be253cc046c4d1164ebfeea
HJYIMN0 <sim0allaseconda@gmail.com>
2025-05-23 19:30:05 +0200

    Update PlayerController.cs

--- Assets/Scenes/Scripts/PlayerController.cs
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D _rb;
     public float speed = 5f;
+    private float h;
+    private float v;
 
     // Start is called before the first frame update
     void Start()
@@ -14,14 +16,22 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
+    private void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+    }
     void FixedUpdate()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        
         if (h != 0 || v != 0)
         {
+
             //Vector2 force = new Vector2 (h, v).normalized * speed;
             //_rb.AddForce(force);
+
+
             Vector2 dir = new Vector2(h, v).normalized;
             if (dir != Vector2.zero)
             {
